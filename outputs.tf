71c6c6695f22c7558847c455244fcd1ceb6ad99d
Daniel Allen <dan@aeternagroup.net>
2026-07-06 23:15:10 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -2,4 +2,12 @@ output "site_recovery_services_vault_hyperv_sites" {
   description = "All site_recovery_services_vault_hyperv_site resources"
   value       = azurerm_site_recovery_services_vault_hyperv_site.site_recovery_services_vault_hyperv_sites
 }
+output "site_recovery_services_vault_hyperv_sites_name" {
+  description = "List of name values across all site_recovery_services_vault_hyperv_sites"
+  value       = [for k, v in azurerm_site_recovery_services_vault_hyperv_site.site_recovery_services_vault_hyperv_sites : v.name]
+}
+output "site_recovery_services_vault_hyperv_sites_recovery_vault_id" {
+  description = "List of recovery_vault_id values across all site_recovery_services_vault_hyperv_sites"
+  value       = [for k, v in azurerm_site_recovery_services_vault_hyperv_site.site_recovery_services_vault_hyperv_sites : v.recovery_vault_id]
+}
 
