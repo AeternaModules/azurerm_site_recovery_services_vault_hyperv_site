933cd32c8f80d18bb128c27ef840e57c439a7faa
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:20:29 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,13 +1,9 @@
-output "site_recovery_services_vault_hyperv_sites" {
-  description = "All site_recovery_services_vault_hyperv_site resources"
-  value       = azurerm_site_recovery_services_vault_hyperv_site.site_recovery_services_vault_hyperv_sites
-}
 output "site_recovery_services_vault_hyperv_sites_name" {
-  description = "List of name values across all site_recovery_services_vault_hyperv_sites"
-  value       = [for k, v in azurerm_site_recovery_services_vault_hyperv_site.site_recovery_services_vault_hyperv_sites : v.name]
+  description = "Map of name values across all site_recovery_services_vault_hyperv_sites, keyed the same as var.site_recovery_services_vault_hyperv_sites"
+  value       = { for k, v in azurerm_site_recovery_services_vault_hyperv_site.site_recovery_services_vault_hyperv_sites : k => v.name }
 }
 output "site_recovery_services_vault_hyperv_sites_recovery_vault_id" {
-  description = "List of recovery_vault_id values across all site_recovery_services_vault_hyperv_sites"
-  value       = [for k, v in azurerm_site_recovery_services_vault_hyperv_site.site_recovery_services_vault_hyperv_sites : v.recovery_vault_id]
+  description = "Map of recovery_vault_id values across all site_recovery_services_vault_hyperv_sites, keyed the same as var.site_recovery_services_vault_hyperv_sites"
+  value       = { for k, v in azurerm_site_recovery_services_vault_hyperv_site.site_recovery_services_vault_hyperv_sites : k => v.recovery_vault_id }
 }
 
