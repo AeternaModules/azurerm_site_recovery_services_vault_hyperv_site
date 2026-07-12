b2db6c4c321f0f2908735bea4fadeb04ec6ef177
Daniel Allen <dan@aeternagroup.net>
2026-07-12 16:05:01 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
+output "site_recovery_services_vault_hyperv_sites_id" {
+  description = "Map of id values across all site_recovery_services_vault_hyperv_sites, keyed the same as var.site_recovery_services_vault_hyperv_sites"
+  value       = { for k, v in azurerm_site_recovery_services_vault_hyperv_site.site_recovery_services_vault_hyperv_sites : k => v.id }
+}
 output "site_recovery_services_vault_hyperv_sites_name" {
   description = "Map of name values across all site_recovery_services_vault_hyperv_sites, keyed the same as var.site_recovery_services_vault_hyperv_sites"
   value       = { for k, v in azurerm_site_recovery_services_vault_hyperv_site.site_recovery_services_vault_hyperv_sites : k => v.name }
